30b2eb500d3166420f5fd76f5fc5feaa6d6ea37c
Dat Nguyen-Tien <118755598+datnguyen-tien204@users.noreply.github.com>
2025-11-10 15:08:29 +0700

    Update __init__.py

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -2,12 +2,9 @@ from pathlib import Path
 import torch
 import torch.utils.data
 from .torchvision_datasets import CocoDetection
-
 from .dataset import build
 from .dataset_fewshot import build as build_fewshot
 
-
-# Meta-settings for few-shot object detection: base / novel category split
 coco_base_class_ids = [
     8, 10, 11, 13, 14, 15, 22, 23, 24, 25, 27, 28, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 46, 47,
     48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 65, 70, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 84,
@@ -41,7 +38,6 @@ voc_base3_class_ids = [
 voc_novel3_class_ids = [
     4, 8, 14, 17, 18
 ]
-
 
 def get_class_ids(dataset, type):
     if dataset == 'coco_base':
@@ -104,7 +100,6 @@ def get_class_ids(dataset, type):
             raise ValueError
     raise ValueError
 
-
 def get_coco_api_from_dataset(dataset):
     for _ in range(10):
         if isinstance(dataset, torch.utils.data.Subset):
@@ -112,11 +107,8 @@ def get_coco_api_from_dataset(dataset):
     if isinstance(dataset, CocoDetection):
         return dataset.coco
 
-
-def build_dataset(image_set, args):
+def build_dataset_original(image_set, args):
     assert image_set in ['train', 'val', 'fewshot'], "image_set must be 'train', 'val' or 'fewshot'."
-    # For training set, need to perform base/novel category filtering.
-    # For training set, we use dataset with support to construct meta-tasks
     if image_set == 'train':
         if args.dataset_file == 'coco':
             root = Path('data/coco')
@@ -150,10 +142,6 @@ def build_dataset(image_set, args):
             img_folder = root / "images"
             ann_file = root / "annotations" / 'pascal_trainval0712.json'
             return build(args, img_folder, ann_file, image_set, activated_class_ids=voc_base3_class_ids, with_support=True)
-
-    # For valid set, no need to perform base/novel category filtering.
-    # This is because that evaluation should be performed on all images.
-    # For valid set, we do not need support dataset.
     if image_set == 'val':
         if args.dataset_file in ['coco', 'coco_base']:
             root = Path('data/coco')
@@ -167,7 +155,6 @@ def build_dataset(image_set, args):
             img_folder = root / "images"
             ann_file = root / "annotations" / 'pascal_test2007.json'
             return build(args, img_folder, ann_file, image_set, activated_class_ids=list(range(1, 20+1)), with_support=False)
-
     if image_set == 'fewshot':
         if args.dataset_file in ['coco', 'coco_base']:
             class_ids = coco_base_class_ids + coco_novel_class_ids
@@ -175,5 +162,28 @@ def build_dataset(image_set, args):
             return build_fewshot(args, image_set, activated_class_ids=class_ids, with_support=True)
         if args.dataset_file in ['voc', 'voc_base1', 'voc_base2', 'voc_base3']:
             return build_fewshot(args, image_set, activated_class_ids=list(range(1, 20+1)), with_support=True)
-
     raise ValueError(f'{image_set} of dataset {args.dataset_file}  not supported.')
+
+def build_dataset(image_set, args):
+    if image_set == 'train' or image_set == 'fewshot':
+        if args.dataset_file == 'zaic_base':
+            root = Path('data/zaic_dataset_coco')
+            img_folder = root / "train2017"
+            ann_file = root / "annotations" / 'train_coco.json'
+            import json
+            with open(str(ann_file), 'r') as f:
+                num_categories = len(json.load(f)['categories'])
+            class_ids = list(range(1, num_categories + 1))
+            return build(args, img_folder, ann_file, 'train', activated_class_ids=class_ids, with_support=True)
+    if image_set == 'val':
+        if args.dataset_file == 'zaic_base':
+            root = Path('data/zaic_dataset_coco')
+            img_folder = root / "val2017"
+            ann_file = root / "annotations" / 'val_coco.json'
+            import json
+            train_ann_file = root / "annotations" / 'train_coco.json'
+            with open(str(train_ann_file), 'r') as f:
+                num_categories_total = len(json.load(f)['categories'])
+            class_ids = list(range(1, num_categories_total + 1))
+            return build(args, img_folder, ann_file, 'val', activated_class_ids=class_ids, with_support=False)
+    return build_dataset_original(image_set, args)
